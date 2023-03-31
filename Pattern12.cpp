f07928f6d9746b4779da5b2b3d9bb9807f63451b
vivek Singh <vivekkumar52522@gmail.com>
2023-03-31 19:37:00 +0530

    Pattern

---
+++ Pattern12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+    cout<<"Enter the Patter Size"<<endl;
+    cin>>n;
+    int k=0;
+    for(int i=1;i<=n;i++){
+        k=1;
+        for(int j=1;j<=2*n-1;j++){
+            if(j>=n-i+1 && j<=n+i-1){
+                cout<<k;
+             j<n?k++:k--;  
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
