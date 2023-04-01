a36912502bebc264006b8bfdb8d29ec013dc9dd2
vivek Singh <vivekkumar52522@gmail.com>
2023-04-01 12:33:59 +0530

    Pattern

---
+++ Pattern24.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+    cout<<"Enter the Patter Size"<<endl;
+    cin>>n;
+    int space=2*(n-1);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1 || j==1 || i==n || j==n){
+            cout<<n;
+            }else{
+                cout<<n-i;
+            }
+        }
+      cout<<endl;
+     
+    }
+}
